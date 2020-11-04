0df27b3ad57dcae8896865fc9e3bae3db4c55cfc
Thomas SIMON <thomas.simon@wishgroupe.com>
2020-11-04 09:08:13 +0100

    resolve #10 Debug probleme de droit dans l'update de mon compte

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,12 @@ Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard
 
 Route::group(['middleware'=>['check_user_role:' . UserRole::ROLE_MEMBRE.',false']],function () {
     Route::get('/my-account/{user}', [ProfileController::class,'edit'])->name('myProfile.my-account');
+    Route::put('/profile/{user}/update', [ProfileController::class,'update'])->name('profile.update');
 });
 
 Route::group(['middleware'=>['check_user_role:' . UserRole::ROLE_GARDIEN]],function () {
     Route::get('/profile/{user}/edit', [ProfileController::class,'edit'])->name('profile.edit');
     Route::get('/profile/{user}/show', [ProfileController::class,'show'])->name('profile.show');
-    Route::put('/profile/{user}/update', [ProfileController::class,'update'])->name('profile.update');
     Route::get('/profile/list', [ProfileController::class,'index'])->name('profile.index');
     Route::get('/profile/new', [ProfileController::class,'create'])->name('profile.create');
     Route::post('/profile/store', [ProfileController::class,'store'])->name('profile.store');
