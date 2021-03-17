a00cfd2ffed3fac30d527e6a2cd3edc96eca7da1
Thomas SIMON <thomas.simon@wishgroupe.com>
2021-03-17 09:27:18 +0100

    add front

---
+++ app/Http/Requests/Admin/Profile/UpdateRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Admin\Profile;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $id = $this->request->get('user_id');
+
+        return  [
+            'name'=> 'required',
+            'email'=> 'required|email|unique:users'. ',id,' . $id,
+            'password'=> 'nullable|min:8',
+            'password_confirmation'=> 'same:password'
+        ];
+    }
+}
---
+++ app/Listeners/ModelCreated.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ModelCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class ModelCreated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ModelCreated  $event
+     * @return void
+     */
+    public function handle(ModelCreated $event)
+    {
+        //
+    }
+}
